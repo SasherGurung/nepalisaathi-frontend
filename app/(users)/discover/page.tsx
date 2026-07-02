b0870ba7a302025d47207f07e4df8741a94c5d4e
Sasher Gurung <sashergrg602@gmail.com>
2026-07-02 12:24:03 +0545

    feat: complete UI for the Discover Page

--- app/(users)/discover/page.tsx
+++ app/(users)/discover/page.tsx
@@ -1,46 +1,138 @@
-import React from 'react'
-import {
-  Card,
-
-} from "@/components/ui/card"
+import React from "react";
+import { Card } from "@/components/ui/card";
 import Image from "next/image";
 import { FaRegCompass } from "react-icons/fa6";
 
 function DiscoverPage() {
   return (
     <section className="min-h-screen flex justify-center">
-        <Card className="w-7xl mt-6">
-            <div className="items-center flex justify-center flex-col">
-              <FaRegCompass className="text-gray-400 w-20 h-20 mb-3" />
-              <div className="flex flex-col justify-center text-center gap-2">
-                <h1 className="text-3xl text-zinc-500 font-bold">Discover New Connection</h1>
-                <p className="line-clamp-3 w-lg text-zinc-500 text-sm">Find people from your hometown, discover new groups, and expand your professional network on Nepali Saathi.</p>
-              </div>
-            </div>
-            <div className="flex justify-center items-center gap-4">
-                <button className="border bg-(--)">
-                  All
-                </button>
-                <button>
-                  Student
-                </button>
-                <button>
-                  Working Professonal
-                </button>
-                <button>
-                  Business Owner
-                </button>
-                <button>
-                  Planning To Relocate
-                </button>
+      <Card className="w-7xl mt-6">
+        <div className="items-center flex justify-center flex-col mb-3">
+          <FaRegCompass className="text-gray-400 w-18 h-18 mb-3" />
+          <div className="flex flex-col justify-center text-center gap-2">
+            <h1 className="text-3xl text-zinc-500 font-bold">
+              Discover New Connection
+            </h1>
+            <p className="line-clamp-3 w-lg text-zinc-500 text-sm">
+              Find people from your hometown, discover new groups, and expand
+              your professional network on Nepali Saathi.
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center items-center gap-3">
+          <button className="border bg-(--brand-maroon) text-white px-4 py-1 rounded-3xl cursor-pointer">
+            All
+          </button>
+          <button className="border bg-zinc-100 text-zinc-500 hover:bg-zinc-200 duration-300 transition-all px-4 py-1 rounded-3xl cursor-pointer">
+            Student
+          </button>
+          <button className="border bg-zinc-100 text-zinc-500 hover:bg-zinc-200 px-4 py-1 rounded-3xl cursor-pointer">
+            Working Professonal
+          </button>
+          <button className="border bg-zinc-100 text-zinc-500 hover:bg-zinc-200 px-4 py-1 rounded-3xl cursor-pointer">
+            Business Owner
+          </button>
+          <button className="border bg-zinc-100 text-zinc-500 hover:bg-zinc-200 px-4 py-1 rounded-3xl cursor-pointer">
+            Planning To Relocate
+          </button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 mt-5">
+          <div className="w-sm rounded-2xl bg-white border border-zinc-200 overflow-hidden transition-all duration-100 hover:shadow-md">
+            <div className="h-20 bg-(--brand-maroon)"></div>
 
-            </div>
-            <div>
+            <div className="flex flex-col items-center px-6 pb-6 -mt-12">
+              <Image
+                src="/logo.png"
+                alt="Profile Image"
+                width={96}
+                height={96}
+                className="w-24 h-24 rounded-full border-4 border-white object-cover shadow-md"
+              />
 
+              <h3 className="mt-3 text-lg font-semibold text-zinc-900">
+                Sasher
+              </h3>
+
+              <p className="text-sm text-zinc-500">Frontend Developer</p>
+
+              <button className="mt-5 w-full rounded-full text-red-600 py-2.5 font-medium border border-red-600 hover:bg-red-100 transition-all cursor-pointer">
+                Connect
+              </button>
             </div>
-        </Card>
+          </div>
+          <div className="w-sm rounded-2xl bg-white border border-zinc-200 overflow-hidden transition-all duration-100 hover:shadow-md">
+            <div className="h-20 bg-(--brand-maroon)"></div>
+
+            <div className="flex flex-col items-center px-6 pb-6 -mt-12">
+              <Image
+                src="/logo.png"
+                alt="Profile Image"
+                width={96}
+                height={96}
+                className="w-24 h-24 rounded-full border-4 border-white object-cover shadow-md"
+              />
+
+              <h3 className="mt-3 text-lg font-semibold text-zinc-900">
+                Sasher
+              </h3>
+
+              <p className="text-sm text-zinc-500">Frontend Developer</p>
+
+              <button className="mt-5 w-full rounded-full text-red-600 py-2.5 font-medium border border-red-600 hover:bg-red-100 transition-all cursor-pointer">
+                Connect
+              </button>
+            </div>
+          </div>
+          <div className="w-sm rounded-2xl bg-white border border-zinc-200 overflow-hidden transition-all duration-100 hover:shadow-md">
+            <div className="h-20 bg-(--brand-maroon)"></div>
+
+            <div className="flex flex-col items-center px-6 pb-6 -mt-12">
+              <Image
+                src="/logo.png"
+                alt="Profile Image"
+                width={96}
+                height={96}
+                className="w-24 h-24 rounded-full border-4 border-white object-cover shadow-md"
+              />
+
+              <h3 className="mt-3 text-lg font-semibold text-zinc-900">
+                Sasher
+              </h3>
+
+              <p className="text-sm text-zinc-500">Frontend Developer</p>
+
+              <button className="mt-5 w-full rounded-full text-red-600 py-2.5 font-medium border border-red-600 hover:bg-red-100 transition-all cursor-pointer">
+                Connect
+              </button>
+            </div>
+          </div>
+          <div className="w-sm rounded-2xl bg-white border border-zinc-200 overflow-hidden transition-all duration-100 hover:shadow-md">
+            <div className="h-20 bg-(--brand-maroon)"></div>
+
+            <div className="flex flex-col items-center px-6 pb-6 -mt-12">
+              <Image
+                src="/logo.png"
+                alt="Profile Image"
+                width={96}
+                height={96}
+                className="w-24 h-24 rounded-full border-4 border-white object-cover shadow-md"
+              />
+
+              <h3 className="mt-3 text-lg font-semibold text-zinc-900">
+                Sasher
+              </h3>
+
+              <p className="text-sm text-zinc-500">Frontend Developer</p>
+
+              <button className="mt-5 w-full rounded-full text-red-600 py-2.5 font-medium border border-red-600 hover:bg-red-100 transition-all cursor-pointer">
+                Connect
+              </button>
+            </div>
+          </div>
+        </div>
+      </Card>
     </section>
-  )
+  );
 }
 
-export default DiscoverPage
+export default DiscoverPage;
